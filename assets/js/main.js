47583787445b41992698f14713d773517e23749e
geocastl <geocastl@umich.edu>
2019-12-30 14:07:10 -0500

    added bobbing anim to play buttons, added white circles, fixed grow divs

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,69 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// This is the important part!
+
+// This is the important part!
+
+function initialCollapse(id) {
+    var element = document.getElementById(id);
+    element.setAttribute('data-collapsed', 'true');
+}
+
+function collapseSection(id) {
+
+  // get the height of the element's inner content, regardless of its actual size
+  var element = document.getElementById(id);
+  var sectionHeight = element.scrollHeight;
+  
+  // temporarily disable all css transitions
+  var elementTransition = element.style.transition;
+  element.style.transition = '';
+  
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we 
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(function() {
+    element.style.height = sectionHeight + 'px';
+    element.style.transition = elementTransition;
+    
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(function() {
+      element.style.height = 3 +'em';
+    });
+  });
+  
+  // mark the section as "currently collapsed"
+  element.setAttribute('data-collapsed', 'true');
+}
+
+function expandSection(id) {
+  
+  // get the height of the element's inner content, regardless of its actual size
+  var element = document.getElementById(id);
+  var sectionHeight = element.scrollHeight;
+  
+  // have the element transition to the height of its inner content
+  element.style.height = sectionHeight + 'px';
+
+  // when the next css transition finishes (which should be the one we just triggered)
+  element.addEventListener('transitionend', function(e) {
+    // remove this event listener so it only gets triggered once
+    element.removeEventListener('transitionend', arguments.callee);
+    
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    element.style.height = null;
+  });
+  
+  // mark the section as "currently not collapsed"
+  element.setAttribute('data-collapsed', 'false');
+}
+
 (function($) {
+
+
+
 
 	var	$window = $(window),
 		$body = $('body'),
